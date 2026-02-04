0815bd93a7b1382360f847e1183c9693f9fa6b98
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-04 14:31:20 -0400

    Add Opportunity model

--- volunteering_app/opportunities/models.py
+++ volunteering_app/opportunities/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Opportunity(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    numTotalVolunteers = models.IntegerField()
+    numSignedVolunteers = models.IntegerField()
+
+    # To keep it simple, hardcode the location for now
+    distanceFromUserKilometers = models.FloatField()
+
+    def __str__(self):
+        return self.title
