2df96c4e9e3a9ed78e2c20ff85480aaf50dc5695
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-07 14:01:26 -0400

    Add template for opportunities app

--- volunteering_app/opportunities/views.py
+++ volunteering_app/opportunities/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Opportunity
+
 
 # Create your views here.
+def opportunities(request):
+    opportunities = Opportunity.objects.all()
+    return render(request, "list.html", {"opportunities": opportunities})
