ed0dbff9a41acf1ccfeecd07ab01ad2c7779df87
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-10 17:49:21 -0400

    Add profile page

--- volunteering_app/users/urls.py
+++ volunteering_app/users/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "users"
+
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
 ]
