781b02287437b099d205e1ad1007da43a3899977
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-10 17:05:49 -0400

    Add nonfunctional login page

--- volunteering_app/users/views.py
+++ volunteering_app/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import MyUserCreationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("opportunities:list")
+    else:
+        form = MyUserCreationForm()
+
+    return render(request, "register.html", {"form": form})
