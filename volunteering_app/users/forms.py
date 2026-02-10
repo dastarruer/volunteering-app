8a4f0e4614abcd1399b36d0bc3431c7e49bfbde4
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-10 17:12:17 -0400

    Remove password field from user form

--- volunteering_app/users/forms.py
+++ volunteering_app/users/forms.py
@@ -6,4 +6,4 @@ from .models import User
 class MyUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ("username", "password")
+        fields = ("username",)
