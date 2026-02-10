41e01a6c25402a6c45bef1360aa6d6f0cca8d907
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-10 17:15:29 -0400

    Use full name instead of username

--- volunteering_app/users/models.py
+++ volunteering_app/users/models.py
@@ -4,9 +4,17 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    full_name = models.CharField(max_length=255, unique=True)
+
+    # Use full_name as username
+    USERNAME_FIELD = "full_name"
+
     upcoming_opportunities = models.ManyToManyField(
         "opportunities.Opportunity", blank=True, related_name="upcoming_by"
     )
     completed_opportunities = models.ManyToManyField(
         "opportunities.Opportunity", blank=True, related_name="completed_by"
     )
+
+    def __str__(self):
+        return self.full_name
