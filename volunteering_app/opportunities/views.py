31451c9f90391565b338c85e037a278e143a4a6c
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-12 18:05:10 -0400

    Only show signed up or completed opportunities

--- volunteering_app/opportunities/views.py
+++ volunteering_app/opportunities/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
 
 from .models import Opportunity
 
 
 # Create your views here.
+@login_required
 def opportunities(request):
-    opportunities = Opportunity.objects.all()
+    signed_up_ids = request.user.upcoming_opportunities.values_list("id", flat=True)
+    completed_ids = request.user.completed_opportunities.values_list("id", flat=True)
+    excluded_ids = list(signed_up_ids) + list(completed_ids)
+
+    # Only show opportunities that the user has not signed up for or completed
+    opportunities = Opportunity.objects.exclude(id__in=excluded_ids)
+
     return render(request, "list.html", {"opportunities": opportunities})
 
 
