f2b1e8aa4baf6fce7f0feadd04bde44da9b0d419
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-12 22:06:40 -0400

    Add streak functionality

--- volunteering_app/users/views.py
+++ volunteering_app/users/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from datetime import timedelta
+
 from django.contrib.auth import login
-from .forms import MyUserCreationForm
-from .models import User
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.utils import timezone
+
+from .forms import MyUserCreationForm
 
 
 def register(request):
@@ -20,11 +23,27 @@ def register(request):
 
 @login_required
 def profile(request):
+    user = request.user
+    last_opportunity = user.completed_opportunities.order_by("-opportunity_date").first()
+
+    if user.streak_last_updated:
+        now = timezone.now()
+        # If it's been more than 7 days, reset streak
+        if now - user.streak_last_updated > timedelta(days=7):
+            user.streak = 0
+            user.save()
+        # If it's been exactly 7 days and the user has completed an opportunity, increment the streak
+        # Yes this logic is bad, and no i will not be fixing it. this is a prototype.
+        elif last_opportunity and user.streak_last_updated == timedelta(days=7):
+            user.streak += 1
+            user.streak_last_updated = now
+
     upcoming_opportunities = request.user.upcoming_opportunities.all()
     completed_opportunities = request.user.completed_opportunities.all()
 
     context = {
         "upcoming_opportunities": upcoming_opportunities,
         "completed_opportunities": completed_opportunities,
+        "streak": user.streak
     }
     return render(request, "profile.html", context)
